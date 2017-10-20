c3e7af0116897a8beb76e61a04c81d7e21b345cd
Andy Hung <andy.o.hung@gmail.com>
2017-10-20 01:01:34 -0400

    Math Class Junit
    Junit testing for math model.

--- app/src/test/java/com/example/BrainGames/BrainGamesComposition/MathTest.java
+++ app/src/test/java/com/example/BrainGames/BrainGamesComposition/MathTest.java
@@ -21,7 +21,29 @@ public class MathTest {
     }
 
     @Test
-    public void generateSum() throws Exception {
+    public void generateSumLevelOne() throws Exception {
+        Math mathObject = new Math(1);
+        mathObject.generateSum();
+        int[] results = mathObject.getNumbers();
+        int first = results[0];
+        int second = results[1];
+        int answer = results[2];
+        assertEquals(answer, first + second);
+    }
+
+    @Test
+    public void generateSumLevelTwo() throws Exception {
+        Math mathObject = new Math(2);
+        mathObject.generateSum();
+        int[] results = mathObject.getNumbers();
+        int first = results[0];
+        int second = results[1];
+        int answer = results[2];
+        assertEquals(answer, first + second);
+    }
+
+    @Test
+    public void generateSumLevelThree() throws Exception {
         Math mathObject = new Math(3);
         mathObject.generateSum();
         int[] results = mathObject.getNumbers();
@@ -32,7 +54,53 @@ public class MathTest {
     }
 
     @Test
-    public void generateSub() throws Exception {
+    public void generateSumLevelFour() throws Exception {
+        Math mathObject = new Math(4);
+        mathObject.generateSum();
+        int[] results = mathObject.getNumbers();
+        int first = results[0];
+        int second = results[1];
+        int answer = results[2];
+        assertEquals(answer, first + second);
+    }
+
+    @Test
+    public void generateSumLevelFive() throws Exception {
+        Math mathObject = new Math(5);
+        mathObject.generateSum();
+        int[] results = mathObject.getNumbers();
+        int first = results[0];
+        int second = results[1];
+        int answer = results[2];
+        assertEquals(answer, first + second);
+    }
+
+    @Test
+    public void generateSubLevelOne() throws Exception {
+
+        Math mathObject = new Math(1);
+        mathObject.generateSub();
+        int[] results = mathObject.getNumbers();
+        int first = results[0];
+        int second = results[1];
+        int answer = results[2];
+        assertEquals(answer, first - second);
+    }
+
+    @Test
+    public void generateSubLevelTwo() throws Exception {
+
+        Math mathObject = new Math(2);
+        mathObject.generateSub();
+        int[] results = mathObject.getNumbers();
+        int first = results[0];
+        int second = results[1];
+        int answer = results[2];
+        assertEquals(answer, first - second);
+    }
+
+    @Test
+    public void generateSubLevelThree() throws Exception {
 
         Math mathObject = new Math(3);
         mathObject.generateSub();
@@ -44,7 +112,53 @@ public class MathTest {
     }
 
     @Test
-    public void generateMul() throws Exception {
+    public void generateSubLevelFour() throws Exception {
+
+        Math mathObject = new Math(4);
+        mathObject.generateSub();
+        int[] results = mathObject.getNumbers();
+        int first = results[0];
+        int second = results[1];
+        int answer = results[2];
+        assertEquals(answer, first - second);
+    }
+
+    @Test
+    public void generateSubLevelFive() throws Exception {
+
+        Math mathObject = new Math(5);
+        mathObject.generateSub();
+        int[] results = mathObject.getNumbers();
+        int first = results[0];
+        int second = results[1];
+        int answer = results[2];
+        assertEquals(answer, first - second);
+    }
+
+    @Test
+    public void generateMulLevelOne() throws Exception {
+        Math mathObject = new Math(1);
+        mathObject.generateMul();
+        int[] results = mathObject.getNumbers();
+        int first = results[0];
+        int second = results[1];
+        int answer = results[2];
+        assertEquals(answer, first * second);
+    }
+
+    @Test
+    public void generateMulLevelTwo() throws Exception {
+        Math mathObject = new Math(2);
+        mathObject.generateMul();
+        int[] results = mathObject.getNumbers();
+        int first = results[0];
+        int second = results[1];
+        int answer = results[2];
+        assertEquals(answer, first * second);
+    }
+
+    @Test
+    public void generateMulLevelThree() throws Exception {
         Math mathObject = new Math(3);
         mathObject.generateMul();
         int[] results = mathObject.getNumbers();
@@ -55,7 +169,51 @@ public class MathTest {
     }
 
     @Test
-    public void generateDiv() throws Exception {
+    public void generateMulLevelFour() throws Exception {
+        Math mathObject = new Math(3);
+        mathObject.generateMul();
+        int[] results = mathObject.getNumbers();
+        int first = results[0];
+        int second = results[1];
+        int answer = results[2];
+        assertEquals(answer, first * second);
+    }
+
+    @Test
+    public void generateMulLevelFive() throws Exception {
+        Math mathObject = new Math(3);
+        mathObject.generateMul();
+        int[] results = mathObject.getNumbers();
+        int first = results[0];
+        int second = results[1];
+        int answer = results[2];
+        assertEquals(answer, first * second);
+    }
+
+    @Test
+    public void generateDivLevelOne() throws Exception {
+        Math mathObject = new Math(1);
+        mathObject.generateDiv();
+        int[] results = mathObject.getNumbers();
+        int first = results[0];
+        int second = results[1];
+        int answer = results[2];
+        assertEquals(answer, first / second);
+    }
+
+    @Test
+    public void generateDivLevelTwo() throws Exception {
+        Math mathObject = new Math(2);
+        mathObject.generateDiv();
+        int[] results = mathObject.getNumbers();
+        int first = results[0];
+        int second = results[1];
+        int answer = results[2];
+        assertEquals(answer, first / second);
+    }
+
+    @Test
+    public void generateDivLevelThree() throws Exception {
         Math mathObject = new Math(3);
         mathObject.generateDiv();
         int[] results = mathObject.getNumbers();
@@ -65,4 +223,25 @@ public class MathTest {
         assertEquals(answer, first / second);
     }
 
+    @Test
+    public void generateDivLevelFour() throws Exception {
+        Math mathObject = new Math(4);
+        mathObject.generateDiv();
+        int[] results = mathObject.getNumbers();
+        int first = results[0];
+        int second = results[1];
+        int answer = results[2];
+        assertEquals(answer, first / second);
+    }
+
+    @Test
+    public void generateDivLevelFive() throws Exception {
+        Math mathObject = new Math(5);
+        mathObject.generateDiv();
+        int[] results = mathObject.getNumbers();
+        int first = results[0];
+        int second = results[1];
+        int answer = results[2];
+        assertEquals(answer, first / second);
+    }
 }
