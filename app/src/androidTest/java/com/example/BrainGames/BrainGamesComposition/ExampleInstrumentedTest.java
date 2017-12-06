5fa21798ec5278ae2920f43f650082b7d25f9e02
Andy Hung <andy.o.hung@gmail.com>
2017-12-06 06:13:16 -0500

    design
    chanaged the button looks for the main act.

--- app/src/androidTest/java/com/example/BrainGames/BrainGamesComposition/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/BrainGames/BrainGamesComposition/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.andyhung.memorygame", appContext.getPackageName());
+        assertEquals("com.example.BrainGames.BrainGamesComposition", appContext.getPackageName());
     }
 }
